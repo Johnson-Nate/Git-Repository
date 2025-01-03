818d71e2774470778d4e9d62009e7aafd7a4ef4c
Johnson-Nate <nate@17consulting-group.com>
2025-01-02 19:46:57 -0800

    Initial commit

---
+++ react-presentation/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DealPresentation from './components/DealPresentation';
+
+function App() {
+  return (
+    <div className="App">
+      <DealPresentation />
+    </div>
+  );
+}
+
+export default App;
